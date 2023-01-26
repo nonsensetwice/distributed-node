06ce799534d9f259161a913f9dcfc683a1bc0f45
nonsense <nonsensetwice@users.noreply.github.com>
2023-01-26 00:34:19 -0800

    ch2 ex 03

---
+++ web-api/consumer-https-basic.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+import fastify from 'fastify';
+import fetch from 'node-fetch';
+import https from 'https';
+import fs from 'fs';
+import path, { resolve } from 'path';
+
+const server = fastify();
+const HOST = process.env.HOST || '127.0.0.1';
+const PORT = process.env.PORT || 3000;
+const TARGET = process.env.TARGET || 'localhost:4000';
+
+const options = {
+	agent: new https.Agent({
+		ca: fs.readFileSync(resolve('/../shared/tls/basic-certificate.cert')),
+	})
+};
+
+server.get('/', async () => {
+    const req = await fetch(`https://${TARGET}/recipes/42`, options);
+    const producer_data = await req.json();
+    return { consumer_pid: process.pid, producer_data };
+});
+server.listen(PORT, HOST, () => {
+    console.log(`Consumer running at http://${HOST}:${PORT}/`);
+});
